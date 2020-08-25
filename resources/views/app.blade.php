c31cf9ce494dd38e0cb9118b03830e223d990127
Geiler Elias Radillo Sarmiento <geilerelias@gmail.com>
2020-08-24 21:48:29 -0500

    actualización de la pagina principal

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,6 +7,15 @@
               content="width=device-width, initial-scale=1">
 
         <title>Soy Maria Victoria Aponte</title>
+        <meta
+            name="description"
+            content="Me inspira el saber de las mujeres, estoy comprometida con su autovaloración. Confiero pasión en el empoderamiento de cada mujer, con objetividad y claridad. Mi travesía personal refleja una combinación de saberes cotidianos, profesionales y sociales, inspirados en la necesidad de despertar conciencias de mujeres."
+        />
+        <meta name="keywords" content="Maria Victoria Aponte Valverde, Empoderamiento"/>
+        <meta name="author" content="Geiler Elias Radillo Sarmiento" />
+
+        <meta name="copyright" content="Laravel - vue - vuetify" />
+        <meta name="robots" content="index"/>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600"
